e1d621a4f9a9e21ae38ef980d9d31e7c59b9cf7c
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-10-16 01:05:53 -0300

    Adding Grid Layout

---
+++ src/Component/Exercises/RightPane.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+
+const LeftPane = ({classes}) => (
+  <Paper className={classes.paper}>
+    Right Pane
+  </Paper>
+)
+
+export default LeftPane;
--- src/Component/Exercises/index.js
+++ src/Component/Exercises/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 
-export default () => (
-  <div>Hello from Exercises</div>
-)
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import LeftPane from './LetPane';
+import RigthPane from './RightPane';
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    padding: 20,
+    marginTop: 10,
+    marginBottom: 10
+  }
+}))
+
+export default () => {
+  const classes = useStyles();
+  return (
+    <Grid container spacing={2}>
+    <Grid item sm>
+     <LeftPane classes={classes} />
+    </Grid>
+    <Grid item sm> 
+      <RigthPane classes={classes} />
+    </Grid>
+
+  </Grid>
+  )
+  
+}
